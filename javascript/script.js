e3fc92fc127689e6feaa0ee61fd7545a9964d5c5
AlpinooO <alpino.cdc@gmail.com>
2024-10-17 17:11:49 +0200

    Modification menu 1

---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+const toggleButton = document.getElementById('toggle-button');
+const overlay = document.getElementById('overlay');
+const menu = document.getElementById('menu');
+const body = document.body;
+
+toggleButton.addEventListener('click', function () {
+
+    if (body.classList.contains('open')) {
+        body.classList.remove('open');
+    } else {
+        body.classList.add('open');
+    }
+});
+
+overlay.addEventListener('click', function () {
+    body.classList.remove('open');
+});
